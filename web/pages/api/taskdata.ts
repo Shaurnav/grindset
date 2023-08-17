025599a42385586a50be67144d3547e9d9c47200
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-08-16 20:17:32 -0700

    removed sample data

--- web/pages/api/taskdata.ts
+++ web/pages/api/taskdata.ts
@@ -97,40 +97,6 @@ const updateTasks = async (state: IData) => {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const sampleData = {
-		tasks: {
-			"task-1": { id: "task-1", description: "Take out the garbage" },
-			"task-2": { id: "task-2", description: "Watch my favorite show" },
-			"task-3": { id: "task-3", description: "Charge my phone" },
-			"task-4": { id: "task-4", description: "Cook dinner" },
-			"task-5": { id: "task-5", description: "I'm cooking dinner" },
-			"task-6": { id: "task-6", description: "Eat food"}
-		},
-		columns: {
-			"column-1": {
-				id: "column-1",
-				title: StatusOptions.UNFINISHED,
-				taskIds: ["task-2", "task-3", "task-4"],
-			},
-			"column-2": {
-				id: "column-2",
-				title: StatusOptions.INPROGRESS,
-				taskIds: ["task-1"],
-			},
-			"column-3": {
-				id: "column-3",
-				title: StatusOptions.REVIEWING,
-				taskIds: ["task-5"],
-			},
-			"column-4": {
-				id: "column-4",
-				title: StatusOptions.DONE,
-				taskIds: ["task-6"],
-			},
-		},
-		columnOrder: ["column-1", "column-2", "column-3", "column-4"]
-	};
-
 	if (req.method === 'PUT') {
 		await updateTasks(req.body);
 	} 
